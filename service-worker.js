d156097999e4745ac6f6fbc7d73ae30f27fe65f9
Krishnan <Lakshmanan.Krishnan@mecklenburgcountync.gov>
2022-10-12 15:35:14 -0400

    bug correction

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@
 workbox.core.setCacheNameDetails( { prefix: "finslive" } );
 
 //Change this value every time before you build
-const LATEST_VERSION = "v3.3.4"
+const LATEST_VERSION = "v3.3.5"
 
 self.addEventListener( "install", event => {
 	console.log( "Service worker is Installing." );
